41ab7fbaeb4ad078d509a10b11f26425a9117fe5
Gridin Egor <egorka.gridin@gmail.com>
2022-05-12 10:09:24 +0300

    delete employees-list-page

--- src/app/lazy-modules/employees/pages/employees-list-page/employees-list-page.component.ts
+++
@@ -1,9 +0,0 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-
-@Component({
-  selector: 'app-employees-list-page',
-  templateUrl: './employees-list-page.component.html',
-  styleUrls: ['./employees-list-page.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
-})
-export class EmployeesListPageComponent {}
