9f1fa6f05ce85dc595892cd45f9528847c135c99
Gridin Egor <egorka.gridin@gmail.com>
2022-05-19 16:53:59 +0300

    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
-import { ChangeDetectionStrategy, Component, OnDestroy } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
 import { Subject } from 'rxjs';
+import {
+  initThemeAction,
+  setDarkThemeAction,
+  setLightThemeAction,
+} from './core/store/theme/theme.actions';
 
 @Component({
   selector: 'app-root',
@@ -7,11 +13,25 @@ import { Subject } from 'rxjs';
   styleUrls: ['./app.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent implements OnDestroy {
+export class AppComponent implements OnInit, OnDestroy {
   private destroy$ = new Subject<void>();
+
+  constructor(private store: Store) {}
+
+  public ngOnInit(): void {
+    this.store.dispatch(initThemeAction());
+  }
 
   public ngOnDestroy(): void {
     this.destroy$.next();
     this.destroy$.complete();
+  }
+
+  public setLight(): void {
+    this.store.dispatch(setLightThemeAction());
+  }
+
+  public setDark(): void {
+    this.store.dispatch(setDarkThemeAction());
   }
 }
