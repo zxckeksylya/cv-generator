d7b1e0b893c151a5526143a38689aa3abe29cce9
Gridin Egor <egorka.gridin@gmail.com>
2022-05-25 16:22:17 +0300

    add interface

---
+++ src/app/core/interfaces/I18nKeyMessageConfig.ts
@@ -0,0 +1,4 @@
+export interface I18nKeyMessageConfig {
+  i18nKey: string;
+  i18nParams?: Record<string, any>;
+}
---
+++ src/app/core/pipes/getControlErrorMessagePipe.ts
@@ -0,0 +1,21 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { I18nKeyMessageConfig } from '../interfaces/I18nKeyMessageConfig';
+
+@Pipe({
+  name: 'getControlErrorMessage',
+})
+export class GetControlErrorMessagePipe implements PipeTransform {
+  public transform(
+    controlErrors: Record<string, any> | null,
+    errorsMap: Record<string, I18nKeyMessageConfig>,
+  ): I18nKeyMessageConfig | null {
+    if (!controlErrors) {
+      return {
+        i18nKey: '',
+      };
+    }
+
+    const key = Object.keys(controlErrors || {})[0];
+    return errorsMap[key] || {};
+  }
+}
