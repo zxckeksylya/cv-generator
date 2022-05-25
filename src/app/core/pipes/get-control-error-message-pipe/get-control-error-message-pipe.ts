017387541061176ad4052b2324d9d5f996058a08
Gridin Egor <egorka.gridin@gmail.com>
2022-05-25 16:28:29 +0300

    add get control error message pipe module

--- src/app/core/pipes/get-control-error-message-pipe/get-control-error-message-pipe.ts
+++ src/app/core/pipes/get-control-error-message-pipe/get-control-error-message-pipe.ts
@@ -1,5 +1,5 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { I18nKeyMessageConfig } from '../interfaces/I18nKeyMessageConfig';
+import { I18nKeyMessageConfig } from '../../interfaces/I18nKeyMessageConfig';
 
 @Pipe({
   name: 'getControlErrorMessage',
