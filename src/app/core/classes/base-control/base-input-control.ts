e8c0ed66b480c6c615aa9304f1dd2684b87722df
Gridin Egor <egorka.gridin@gmail.com>
2022-06-06 16:34:56 +0300

    delete

--- src/app/core/classes/base-control/base-input-control.ts
+++
@@ -1,8 +0,0 @@
-import { BaseControl } from './base-control.class';
-import { Directive, Input } from '@angular/core';
-
-@Directive()
-export class BaseInputControl extends BaseControl {
-  @Input() public label = '';
-  @Input() public placeholder = '';
-}
