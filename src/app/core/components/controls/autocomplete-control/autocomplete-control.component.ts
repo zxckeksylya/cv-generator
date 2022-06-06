c6ade014f8ca5f1c4e2d53873ac3a8848f476bce
Gridin Egor <egorka.gridin@gmail.com>
2022-06-06 16:35:36 +0300

    fix autocontrol

--- src/app/core/components/controls/autocomplete-control/autocomplete-control.component.ts
+++ src/app/core/components/controls/autocomplete-control/autocomplete-control.component.ts
@@ -1,15 +1,25 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { BaseInputControl } from 'src/app/core/classes/base-control/base-input-control.class';
 
+export interface Data {
+  label: string;
+  value: string;
+}
+
 @Component({
   selector: 'app-autocomplete-control',
   templateUrl: './autocomplete-control.component.html',
-  styleUrls: [
-    './autocomplete-control.component.scss',
-    '../../../../../assets/styles/base-control.styles.scss',
-  ],
+  styleUrls: ['./autocomplete-control.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AutocompleteControlComponent extends BaseInputControl {
-  @Input() public dataSource = [];
+  @Input() public dataSource: Data[] = [];
+
+  public compareFun = (o1: Data | string, o2: Data): boolean => {
+    if (o1) {
+      return typeof o1 === 'string' ? o1 === o2.label : o1.value === o2.value;
+    } else {
+      return false;
+    }
+  };
 }
