cbe8ed1da1b30a0f7df2bb33f21deec0ac40c2e2
Gridin Egor <egorka.gridin@gmail.com>
2022-06-14 13:04:06 +0300

    remove useless modules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,7 @@
-import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { Subject } from 'rxjs';
-import { Themes } from './core/enums/themes';
 import { AppState } from './core/store/app.reducers';
-import { changeThemeAction, initThemeAction } from './core/store/theme/theme.actions';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { initThemeAction } from './core/store/theme/theme.actions';
 
 @Component({
   selector: 'app-root',
@@ -12,41 +9,10 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
   styleUrls: ['./app.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent implements OnInit, OnDestroy {
-  public fg: FormGroup;
-  private destroy$ = new Subject<void>();
-
-  constructor(private store: Store<AppState>, private formBuilder: FormBuilder) {}
+export class AppComponent implements OnInit {
+  constructor(private store: Store<AppState>) {}
 
   public ngOnInit(): void {
     this.store.dispatch(initThemeAction());
-    this.initForm();
-  }
-
-  public ngOnDestroy(): void {
-    this.destroy$.next();
-    this.destroy$.complete();
-  }
-
-  public setLight(): void {
-    this.store.dispatch(changeThemeAction({ theme: Themes.LIGHT }));
-  }
-
-  public setDark(): void {
-    this.store.dispatch(changeThemeAction({ theme: Themes.DARK }));
-  }
-  public submitForm(): void {
-    if (this.fg.valid) {
-      this.fg.reset();
-      //this.fg.get('control')?.markAsPristine();
-    }
-  }
-  private initForm(): void {
-    this.fg = this.formBuilder.group({
-      control: ['', [Validators.required]],
-      textarea: [''],
-      autocomplite: [''],
-      datePicker: [''],
-    });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,13 @@ import { registerLocaleData } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import en from '@angular/common/locales/en';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { en_US, NZ_I18N } from 'ng-zorro-antd/i18n';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppTranslateModule } from './core/app-translate/app-translate.module';
-import { TextControlModule } from './core/components/controls/text-control/text-control.module';
-import { TextareaControlModule } from './core/components/controls/textarea-control/textarea-control.module';
 import { AppStoreModule } from './core/store/app-store.module';
-import { AutocompleteControlModule } from './core/components/controls/autocomplete-control/autocomplete-control.module';
-import { ButtonModule } from './core/components/button/button.module';
-import { DatePickerControlModule } from './core/components/controls/date-picker-control/date-picker-control.module';
 
 registerLocaleData(en);
 
@@ -23,17 +17,10 @@ registerLocaleData(en);
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule,
-    FormsModule,
     HttpClientModule,
     BrowserAnimationsModule,
     AppStoreModule,
     AppTranslateModule,
-    TextControlModule,
-    TextareaControlModule,
-    AutocompleteControlModule,
-    ButtonModule,
-    DatePickerControlModule,
   ],
   providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent],
