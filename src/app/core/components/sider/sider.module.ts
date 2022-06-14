8ad8efbc3a7188bba8b629202b8a69439d24028b
Gridin Egor <egorka.gridin@gmail.com>
2022-06-14 17:05:39 +0300

    fix sider

--- src/app/core/components/sider/sider.module.ts
+++ src/app/core/components/sider/sider.module.ts
@@ -4,8 +4,9 @@ import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { SiderComponent } from './sider.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { AppTranslateModule } from '../../app-translate/app-translate.module';
 @NgModule({
-  imports: [NzLayoutModule, CommonModule, FontAwesomeModule, NzMenuModule],
+  imports: [NzLayoutModule, CommonModule, FontAwesomeModule, NzMenuModule, AppTranslateModule],
   declarations: [SiderComponent],
   exports: [SiderComponent],
 })
