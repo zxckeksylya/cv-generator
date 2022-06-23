2eb45c145e3e2d3ffe65828e51580f51b623d0bf
Gridin Egor <egorka.gridin@gmail.com>
2022-06-23 20:22:23 +0300

    remove useless store

--- src/app/core/store/alert/alert.actions.ts
+++
@@ -1,3 +0,0 @@
-import { createAction, props } from '@ngrx/store';
-
-export const setAlertsAction = createAction('[ALERT] set alerts', props<{ errors: string[] }>());
--- src/app/core/store/alert/alert.selectors.ts
+++
@@ -1,6 +0,0 @@
-import { createFeatureSelector, createSelector } from '@ngrx/store';
-import { AlertState, ALERT_FEATURE_KEY } from './alert.reducers';
-
-export const alertFeatureSelector = createFeatureSelector<AlertState>(ALERT_FEATURE_KEY);
-
-export const alertSelector = createSelector(alertFeatureSelector, (state) => state.errors);
