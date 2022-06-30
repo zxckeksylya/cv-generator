c911518fc1074f99b140fe9858fd85e9a85b5462
Gridin Egor <egorka.gridin@gmail.com>
2022-06-30 13:35:59 +0300

    fix comments

--- src/app/lazy-modules/projects/projects-routing.module.ts
+++ src/app/lazy-modules/projects/projects-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ProjectsListPageComponent } from './pages/projects-list-page/projects-list-page.component';
 import { ProjectFormPageComponent } from './pages/project-form-page/project-form-page.component';
+import { ProjectsListPageComponent } from './pages/projects-list-page/projects-list-page.component';
 
 const routes: Routes = [
   {
