912ced4a78bd2712a223ffebf0f26f054422bb10
Gridin Egor <egorka.gridin@gmail.com>
2022-07-07 18:10:25 +0300

    remove useless modal component

--- src/app/lazy-modules/main/main.module.ts
+++ src/app/lazy-modules/main/main.module.ts
@@ -1,17 +1,17 @@
 import { NgModule } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzModalModule } from 'ng-zorro-antd/modal';
+import { BreadcrumbModule } from 'src/app/core/components/breadcrumb/breadcrumb.module';
+import { ButtonModule } from '../../core/components/button/button.module';
+import { DropdownModule } from '../../core/components/dropdown/dropdown.module';
+import { LanguageButtonModule } from '../../core/components/language-button/language-button.module';
+import { PageHeadingModule } from '../../core/components/page-heading/page-heading.module';
+import { SiderModule } from '../../core/components/sider/sider.module';
+import { HeaderComponent } from './components/header/header.component';
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
 import { MainRoutingModule } from './main-routing.module';
 import { MainPageComponent } from './pages/main-page/main-page.component';
-import { SiderModule } from '../../core/components/sider/sider.module';
-import { NzLayoutModule } from 'ng-zorro-antd/layout';
-import { HeaderComponent } from './components/header/header.component';
-import { PageHeadingModule } from '../../core/components/page-heading/page-heading.module';
-import { BreadcrumbModule } from 'src/app/core/components/breadcrumb/breadcrumb.module';
-import { ModalModule } from 'src/app/core/components/modal/modal.module';
-import { LanguageButtonModule } from '../../core/components/language-button/language-button.module';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { UserProfileComponent } from './components/user-profile/user-profile.component';
-import { DropdownModule } from '../../core/components/dropdown/dropdown.module';
-import { ButtonModule } from '../../core/components/button/button.module';
 
 @NgModule({
   imports: [
@@ -20,11 +20,11 @@ import { ButtonModule } from '../../core/components/button/button.module';
     NzLayoutModule,
     PageHeadingModule,
     BreadcrumbModule,
-    ModalModule,
     LanguageButtonModule,
     FontAwesomeModule,
     DropdownModule,
     ButtonModule,
+    NzModalModule,
   ],
   declarations: [MainPageComponent, HeaderComponent, UserProfileComponent],
 })
