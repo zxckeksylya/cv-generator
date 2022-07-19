a48bec39f088abda6395cbdafa1cc94fcd332826
Gridin Egor <egorka.gridin@gmail.com>
2022-07-19 13:42:44 +0300

    refactor delete in map

--- src/app/core/utils/delete-in-map.util.ts
+++ src/app/core/utils/delete-in-map.util.ts
@@ -1,7 +1,5 @@
-export const deleteInMap = <T extends { [id: string]: any }>(map: T, id: string): T =>
-  Object.values(map).reduce((acc, key) => {
-    if (key.id !== id) {
-      acc[key.id] = map[key.id];
-    }
-    return acc;
-  }, {} as T);
+export const deleteInMap = <T extends { [id: string]: any }>(map: T, id: string): T => {
+  const newMap = { ...map };
+  delete newMap[id];
+  return newMap;
+};
